74d2bac90acbb9528c41bcd0f90a7f059100129c
OliwiaGłodek <126956455+oliwiaglodek@users.noreply.github.com>
2024-08-03 22:06:35 +0200

    Logowanie

---
+++ MuzeumInz/MuzeumInz/DbConnect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace MuzeumInz
+{
+    public class DbConnect
+    {
+        private string connectionString;
+
+        public DbConnect()
+        {
+            connectionString = "Data Source=Muzeum.db;Version=3;";
+        }
+
+        public DataTable GetData(/*string query*/ SQLiteCommand command)
+        {
+            DataTable dt = new DataTable();
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                //SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Connection = connection;
+                connection.Open();
+
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                adapter.Fill(dt);
+            }
+
+            return dt;
+        }
+        public void ExecuteQuery(string query)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ MuzeumInz/MuzeumInz/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SQLite;
+using System.Configuration;
+
+namespace MuzeumInz
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DbConnect dbConnect;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dbConnect = new DbConnect();
+        }
+
+        //zamknięcie okna na "X"
+        private void exitClick_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string email = loginTxt.Text;
+            string password = loginPassTxt.Text;
+
+            string query = $"SELECT * FROM users WHERE email='{email}' AND password='{password}'";
+
+            SQLiteCommand command = new SQLiteCommand(query);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@password", password);
+
+            DataTable result = dbConnect.GetData(command);
+
+            if (result.Rows.Count > 0)
+            {
+                MessageBox.Show("Logowanie poprawne");
+            }
+            else
+            {
+                MessageBox.Show("Błedy login lub hasło");
+
+            }
+
+        }
+    }
+
+}
