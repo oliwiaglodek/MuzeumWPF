a5117598a4bd9f5f240b447b45d33dce257fc00c
OliwiaGłodek <126956455+oliwiaglodek@users.noreply.github.com>
2024-08-04 20:26:50 +0200

    Rejestracja 2

--- MuzeumInz/MuzeumInz/DbConnect.cs
+++ MuzeumInz/MuzeumInz/DbConnect.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace MuzeumInz
 {
@@ -14,16 +16,15 @@ namespace MuzeumInz
 
         public DbConnect()
         {
-            connectionString = "Data Source=Muzeum.db;Version=3;";
+            connectionString =ConfigurationManager.ConnectionStrings["Muzeum"].ConnectionString;//"Data Source=Muzeum.db;";
         }
 
-        public DataTable GetData(/*string query*/ SQLiteCommand command)
+        public DataTable GetData(SQLiteCommand command)
         {
             DataTable dt = new DataTable();
 
             using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
-                //SQLiteCommand command = new SQLiteCommand(query, connection);
                 command.Connection = connection;
                 connection.Open();
 
@@ -39,6 +40,41 @@ namespace MuzeumInz
             {
                 SQLiteCommand command = new SQLiteCommand(query, connection);
                 connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+
+        //sprawdzenie czy użytkonik istanieje w bazie - do rejestracji
+        public bool CheckUserExist(string email)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM users WHERE email = @email";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@email", email);
+
+                int userCount = Convert.ToInt32(command.ExecuteScalar());
+                return userCount > 0;
+            }
+        }
+
+        //dodanie użytkownika
+        public void RegisterUser(string email, string password)
+        {
+            using (SQLiteConnection connection = GetConnection())
+            {
+                connection.Open();
+                string query = "INSERT INTO users (email, password) VALUES (@email, @password)";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@email", email);
+                command.Parameters.AddWithValue("@password", password);
+
                 command.ExecuteNonQuery();
             }
         }
